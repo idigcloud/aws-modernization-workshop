b174d623e9740d33e21db275e9560266ddfa4553
jayyeras <jayyeras@amazon.com>
2018-12-04 12:13:36 -0800

    refactor
    Signed-off-by: jayyeras <jayyeras@amazon.com>

--- modules/create-environment/functions/source/setenv.py
+++ modules/create-environment/functions/source/setenv.py
@@ -1,4 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# setenv.py
+#
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##################################################################################################
+
 from __future__ import print_function
+
 import time
 import boto3
 import crhelper
@@ -46,9 +66,7 @@ def create(event, context):
     print("Received request to {}.".format(event['RequestType']))
     # Open AWS clients
 
-    instance = get_instance('{}{}{}{}'.format(
-                            'aws-cloud9-', event['ResourceProperties']['StackName'],
-                            '-', event['ResourceProperties']['EnvironmentId']))
+    instance = get_instance('{}{}{}{}'.format('aws-cloud9-', event['ResourceProperties']['StackName'],'-', event['ResourceProperties']['EnvironmentId']))
 
     instance_name = instance['InstanceId']
 
@@ -65,10 +83,8 @@ def create(event, context):
 
     # Modify the size of the Cloud9 IDE EBS volume
     client.get_waiter('instance_status_ok').wait(InstanceIds=[instance['InstanceId']])
-    print("Resizing volume {} for instance {} to {}. This will take several minutes to complete.".format(
-        instance['BlockVolumeId'], instance['InstanceId'], event['ResourceProperties']['EBSVolumeSize']))
-    client.modify_volume(VolumeId=block_volume_id,
-                         Size=int(event['ResourceProperties']['EBSVolumeSize']))
+    print("Resizing volume {} for instance {} to {}. This will take several minutes to complete.".format(block_volume_id,instance['InstanceId'],event['ResourceProperties']['EBSVolumeSize']))
+    client.modify_volume(VolumeId=block_volume_id,Size=int(event['ResourceProperties']['EBSVolumeSize']))
 
     # Reboot the Cloud9 IDE
     volume_state = client.describe_volumes_modifications(VolumeIds=[block_volume_id])['VolumesModifications'][0]
